109f13a0df792c999663b98f2b470dc1b7e19550
SFerFer1 <10048@holbertonstudents.com>
2024-10-17 17:21:20 +0000

    try number 7 in  c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,6 +4,8 @@ main - Write script,
  */
 int main(void)
 {
+char *variabletype[] = {"char", "unsigned char", "signed char", "int", "unsigned int", "short", "unsigned short", "long", "unsigned long", "long long", "unsigned long long"};
+
         for (int i = 0; i < 11;++i) {
         printf("Size of %s is %u\n",variabletype[i], (unsigned int)(sizeof(variabletype[i])));
     }
