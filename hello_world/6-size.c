aba435e1e6514c9644877a69e3030c7e20d506e3
SFerFer1 <10048@holbertonstudents.com>
2024-10-17 17:18:48 +0000

    try number 7 in  c

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/*
+main - Write script, 
+ */
+int main(void)
+{
+        for (int i = 0; i < 11;++i) {
+        printf("Size of %s is %u\n",variabletype[i], (unsigned int)(sizeof(variabletype[i])));
+    }
+        return (0);
+}
