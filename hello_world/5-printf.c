e4d336b1d2477f01c201b64d0b9d59832b186894
SFerFer1 <10048@holbertonstudents.com>
2024-10-17 17:11:21 +0000

    try number 6 in  c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -4,6 +4,6 @@ main - Write script,
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,");
+        printf("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
