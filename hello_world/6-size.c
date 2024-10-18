3852b79a29605cfc0228bf26d8187742d6f78834
SFerFer1 <10048@holbertonstudents.com>
2024-10-18 14:17:30 +0000

    c proyect 2

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,19 +1,26 @@
 i#include<stdio.h>
-
+/**
+ * main - cosa rara
+ *
+ *  Return: Always 0 (Success)
+ */
 
 int main(void)
 {
-char *variabletype[] = {"char",   "int",  "short",  "long",  "long long"};
-
-gcc -m32 6-size.c -o size32
 
 
-    for (i = 0; i < 11; ++i) {
-        
-        printf("Size of %s is %zu\n", variabletype[i], sizeof(variabletype[i]));
-    }        
-
-
-
-        return (0);
+#ifdef __x86_64__
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+#elif __i386__
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+#endif
+	return (0);
 }
