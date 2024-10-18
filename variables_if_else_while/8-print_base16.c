929e89ef4b7a18a5d0976147cc381cc9e36d9660
SFerFer1 <10048@holbertonstudents.com>
2024-10-18 17:48:15 +0000

    c proyect 2

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - hexadecimal
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 15; num++)
+	{
+	printf("%x", num);
+	}
+	printf("\n");
+	return (0);
+}
