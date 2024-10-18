e25d53abf2ca7eccbe5c1e7993d28130c06570db
SFerFer1 <10048@holbertonstudents.com>
2024-10-18 13:55:08 +0000

    c proyect 2

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-/*
-main - Write script, 
+/**
+ * main - script with print
+ *
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
-        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
