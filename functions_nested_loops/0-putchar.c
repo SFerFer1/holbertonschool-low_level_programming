52add9a00184dee0f54d4320464bf5c2def8b551
SFerFer1 <10048@holbertonstudents.com>
2024-10-21 23:19:22 +0000

    help

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,31 +1,6 @@
 #include <unistd.h>
+#include "main.h"
 
-/**
- * _putchar - print _putchar to
- *
- *  Return:  0 always
- */
-void _putchar(char c)
-{
-	write(1, &c, 1);
-}
-/**
- * print_putchar - estructure
- *
- *  Return:  0
- */
-void print_putchar(void)
-{
-	char string[] = "_putchar";
-
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-	{
-	_putchar(string[i]);
-	}
-	_putchar('\n');
-}
 /**
  * main - print _putchar
  *
@@ -42,5 +17,6 @@ int main(void)
         _putchar('h');
         _putchar('a');
         _putchar('r');
+	_putchar('\n');i
 	return (0);
 }
