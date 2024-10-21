50058e0b05a0dd6d3b8582bfbb0b1870a9dd2a3e
SFerFer1 <10048@holbertonstudents.com>
2024-10-21 16:50:38 +0000

    first try

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void print_alphabet(void);
-void print_alphabet_x10(void); // Declare the new function
+void print_alphabet(void); /* Declare the function to print the alphabet */
+void print_alphabet_x10(void); /* Declare the function to print the alphabet 10 times */
+int _putchar(char c); /* Declare the _putchar function */
 
 #endif /* MAIN_H */
 /
