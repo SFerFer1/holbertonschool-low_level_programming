48b6641d2491ef1024a4e761be472e22cbd44067
SFerFer1 <10048@holbertonstudents.com>
2024-10-21 22:21:10 +0000

    help

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -34,6 +34,13 @@ void print_putchar(void)
 int main(void)
 {
 
-	print_putchar();
+        _putchar('_');
+	_putchar('p');
+	_putchar('u');
+        _putchar('t');
+        _putchar('c');
+        _putchar('h');
+        _putchar('a');
+        _putchar('r');
 	return (0);
 }
