16e277fa0412deede61b08b58cca9d71dd7b54e9
SFerFer1 <10048@holbertonstudents.com>
2024-10-22 15:30:48 +0000

    works

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,21 +11,12 @@
 int print_last_digit(int n )
 {
 
-if (n == INT_MIN)
-    {
-        
-        return 8;
-    }
-
-
-
 if (n < 0)
 {
 n = n * -1;
 }
  n = n % 10; 
  _putchar(n + '0');
-
 
 return (n);
 }
