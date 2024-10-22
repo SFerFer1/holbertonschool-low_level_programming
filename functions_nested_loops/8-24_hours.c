deffacc72b92edbf7deea8aea2e5448ea8436aa1
SFerFer1 <10048@holbertonstudents.com>
2024-10-22 15:51:38 +0000

    works

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -9,5 +9,19 @@
 void jack_bauer(void)
 {
 
+	int hour;
+	int minutes;
 
+	for (hour = 0; hour < 24 ; hour++)
+	{
+	for (minutes = 0; minutes < 60 ; minutes++)
+	{
+	_putchar(hour / 10 + '0');
+	_putchar(hour % 10 + '0');
+	_putchar(':');
+	_putchar(minute / 10 + '0');
+	_putchar(minute % 10 + '0');
+	_putchar('\n');
+	}
+	}
 }
