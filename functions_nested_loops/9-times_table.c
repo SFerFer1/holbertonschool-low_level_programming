c64db99655f32680e13900ac440436221de6dd1e
SFerFer1 <10048@holbertonstudents.com>
2024-10-22 17:29:09 +0000

    works

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  times_table - prints time table
+ *
+ *  Return: void
+ */
+
+void times_table(void)
+{
+int length;
+int multiplo;
+int n=0;
+for(length=0 ; length <= 9;legth++)
+
+for(multiplo=0 ; multiplo <= 10; multiplo;)
+{
+_putchar((n * multiplo) + '0');
+
+}
+n++
+_putchar('n/');
+}	
