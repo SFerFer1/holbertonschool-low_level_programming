7f0782f186c3eb2796cb48ba24d0dc2fd945d191
SFerFer1 <10048@holbertonstudents.com>
2024-10-22 13:19:45 +0000

    works

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,13 @@
 void print_alphabet_x10(void){
 
 
-char i = 'a';
+
 
 int n=0;
 
 for (n = 0 ; n < 10 ; i++)
 	{
-for (i = 'a'; i <= 'z'; i++)
-{
-	_putchar(i);
-};
+print_alphabet();
 _putchar('\n');
 	}
 	}
