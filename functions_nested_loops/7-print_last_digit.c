700e65bbce419995832d2d238584a1b26c604f0c
SFerFer1 <10048@holbertonstudents.com>
2024-10-23 14:39:23 +0000

    works

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,29 +2,20 @@
 #include <limits.h>
 #include "main.h"
 /**
- *  print_last_digit - return las digit 
+ *  print_last_digit - return las digit
  *
  * @n: int n es un numero cualquiera
  *
- *  Return: las digit 
+ *  Return: las digit
  */
-
-int print_last_digit(int n )
+int print_last_digit(int n)
 {
 	int last = n % 10;
 
-
-
-if (last < 0)
-{
-last = last * -1;
-}
- 
-
-
-
- 
-_putchar(last + '0' );
-
+	if (last < 0)
+	{
+	last = last * -1;
+	}
+	_putchar(last + '0');
 return (last);
 }
