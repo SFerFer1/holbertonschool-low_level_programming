df9b5e2aa0f3d5c31b17ef1fed2ac367dd2d747d
SFerFer1 <10048@holbertonstudents.com>
2024-10-23 12:42:35 +0000

    works

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,34 +10,35 @@ void times_table(void)
 {
 int length;
 int multiplo;
-int n=0;
+int n = 0;
 int resultado;
-for(length=0 ; length <= 9;length++)
+for (length = 0 ; length <= 9; length++)
 {
 
-for(multiplo=0 ; multiplo < 10; multiplo++)
+for (multiplo = 0 ; multiplo < 10; multiplo++)
 {
-	resultado =n * multiplo; 
-if (resultado>= 10 && resultado <= 99)
+	resultado = n * multiplo;
+if (resultado >= 10 && resultado <= 99)
 {
 	_putchar('0' + (resultado / 10));
 	_putchar('0' + (resultado % 10));
 }
 else
 {
-if(multiplo != 0 ){
+if (multiplo != 0)
+{
 _putchar(' ');
 }
-_putchar('0'+ (resultado));
+_putchar('0' + (resultado));
 }
-if(multiplo != 9 )
-        {
-        _putchar(',');
-        _putchar(' ');
-        }
+if (multiplo != 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
 }
 _putchar('\n');
 n++;
 }
 
-}	
+}
