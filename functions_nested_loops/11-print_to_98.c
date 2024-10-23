fadf1120d777cdbba4ad8b4947aee80a25383569
SFerFer1 <10048@holbertonstudents.com>
2024-10-23 13:11:39 +0000

    works

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  add - hace el calculo con las variables recibidas
+ *
+ *@a: int a es un numero para sumar
+ *@b: int a es un numero para sumar
+ *
+ *  Return: int un resultado
+ */
+
+void print_to_98(int n)
+{
+
+valor = n
+
+
+for (inicio = valor ; multiplo <= 98; inicio++)
+{
+
+
+	printf("%d", inicio);
+	_putchar(',');
+	_putchar(' ');
+
+}
+
+
+
+
+}
