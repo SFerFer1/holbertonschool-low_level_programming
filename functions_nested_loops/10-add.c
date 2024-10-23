1fb24e5ebf4d01b513122fb3dbcfcf32666da8de
SFerFer1 <10048@holbertonstudents.com>
2024-10-23 12:53:32 +0000

    works

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *  add - hace el calculo con las variables recibidas
+ *
+ *@n: int n es un numero cualquiera
+ *
+ *  Return: int un resultado
+ */
+int add(int a , int b )
+{
+int resultado;
+
+resultado = a + b
+
+
+
+
+return (resultado)
+}
