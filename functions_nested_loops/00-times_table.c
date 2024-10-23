3fd6588fc067d6fd0496c185f66721b305c776d1
SFerFer1 <10048@holbertonstudents.com>
2024-10-23 15:07:44 +0000

    works

---
+++ functions_nested_loops/00-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+/**
+ *  times_table - prints time table
+ *
+ *  Return: void
+ */
+void print_times_table(int n)
+{
+int length;
+int multiplo;
+int resultado;
+
+if (n < 15 && n > 0){
+for (length = 0 ; length <= n; length++)
+{
+
+for (multiplo = 0 ; multiplo < n; multiplo++)
+{
+        resultado = n * multiplo;
+if (resultado >= 10 && resultado <= 99)
+{
+        _putchar('0' + (resultado / 10));
+        _putchar('0' + (resultado % 10));
+}
+else if (resultado >= 100 && resultado <= 999)
+{
+_putchar('0' + (resultado / 10));
+_putchar('0' + (resultado / 10) % 10);
+_putchar('0' + (resultado % 10));
+}
+
+else
+{
+if (multiplo != 0)
+{
+_putchar(' ');
+}
+_putchar('0' + (resultado));
+}
+if (multiplo != 9)
+        {
+        _putchar(',');
+        _putchar(' ');
+        }
+}
+_putchar('\n');
+n++;
+}
+}
+}
