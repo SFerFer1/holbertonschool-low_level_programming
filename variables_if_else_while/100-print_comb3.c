6b998f516d995518bb24d81fa1256c1ff1be4d23
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 17:20:11 +0000

    initial change

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - combination of 2 digits
+*
+*  Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int n;
+
+for (i = 0; i < 10; i++)
+{
+if(n != i)
+{
+for (n=0; i <)
+	{
+	if(n != i)
+		{
+		putchar (i + '0');
+		putchar (n + '0');	
+
+		if (i != 9)
+			{
+			putchar (',');
+			putchar(' ');
+			}
+		};
+	};
+
+return (0);
+}
