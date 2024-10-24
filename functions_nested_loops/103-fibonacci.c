9f7ca122a29c4343a539b0fac0a20c9c276459fb
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 13:54:31 +0000

    second try

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -7,27 +7,24 @@
  */
 int main(void)
 {
-    long valoractual = 1;   // Primer número de Fibonacci
-    long valoranterior = 2; // Segundo número de Fibonacci
+    long valoractual = 1;   
+    long valoranterior = 2; 
     long cambiovalor;
-    long valortotal = 0;    // Inicializa la suma en 0
+    long valortotal = 0;    
 
-    // Bucle para calcular números de Fibonacci hasta 4,000,000
     while (valoractual <= 4000000)
     {
-        // Verifica si el número actual es par
+        
         if (valoractual % 2 == 0)
         {
-            valortotal += valoractual; // Suma solo los números pares
+            valortotal += valoractual;
         }
 
-        // Calcula el siguiente número de Fibonacci
-        cambiovalor = valoractual + valoranterior; // Siguiente número
-        valoractual = valoranterior; // Mueve valoractual al siguiente
-        valoranterior = cambiovalor;  // Actualiza valoranterior
+        cambiovalor = valoractual + valoranterior;
+        valoractual = valoranterior;
+        valoranterior = cambiovalor;
     }
 
-    // Imprime la suma total de los números de Fibonacci pares
     printf("%ld\n", valortotal);
 
     return (0);
