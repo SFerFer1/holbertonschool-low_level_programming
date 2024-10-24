8eba178578079c62438844e0d76f084096f9e02c
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 13:57:44 +0000

    second try

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -7,26 +7,22 @@
  */
 int main(void)
 {
-    long valoractual = 1;   
-    long valoranterior = 2; 
-    long cambiovalor;
-    long valortotal = 0;    
+	long valoractual = 1;
+	long valoranterior = 2;
+	long cambiovalor;
+	long valortotal = 0;
 
-    while (valoractual <= 4000000)
-    {
-        
-        if (valoractual % 2 == 0)
-        {
-            valortotal += valoractual;
-        }
-
-        cambiovalor = valoractual + valoranterior;
-        valoractual = valoranterior;
-        valoranterior = cambiovalor;
-    }
-
-    printf("%ld\n", valortotal);
-
-    return (0);
+	while (valoractual <= 4000000)
+	{
+	if (valoractual % 2 == 0)
+	{
+	valortotal += valoractual;
+	}
+	cambiovalor = valoractual + valoranterior;
+	valoractual = valoranterior;
+	valoranterior = cambiovalor;
+	}
+	printf("%ld\n", valortotal);
+	return (0);
 }
 
