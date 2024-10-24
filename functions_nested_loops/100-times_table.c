20f6c6409c9fecb752a3bd7cbb34d52326631be9
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 12:05:18 +0000

    second try

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -7,29 +7,34 @@
  */
 void print_times_table(int n)
 {
-    int length, multiplo, resultado;
+	int length, multiplo, resultado;
 
-    if (n < 0 || n > 15)
-        return;
+	if (n < 0 || n > 15)
+	return;
 
-    for (length = 0; length <= n; length++)
-    {
-        for (multiplo = 0; multiplo <= n; multiplo++)
-        {
-            resultado = length * multiplo;
+	for (length = 0; length <= n; length++)
+	{
+	for (multiplo = 0; multiplo <= n; multiplo++)
+	{
+	resultado = length * multiplo;
 
-            if (multiplo != 0)
-                _putchar(',');
-	 if (multiplo == 0 )
-	 _putchar('0'); 
-	    else if (resultado < 10 )
-        _putchar(' '),_putchar(' '), _putchar(' '), _putchar('0' + resultado);
-            else if (resultado < 100)
-                _putchar(' '),_putchar(' '), _putchar('0' + resultado / 10), _putchar('0' + resultado % 10);
-            else
-       _putchar(' '),_putchar('0' + resultado / 100), _putchar('0' + (resultado / 10) % 10), _putchar('0' + resultado % 10);
-        }
-        _putchar('\n');
-    }
+	if (multiplo != 0)
+	_putchar(',');
+	if (multiplo == 0)
+	_putchar('0');
+	else if (resultado < 10)
+		_putchar(' '), _putchar(' '), _putchar(' '), _putchar('0' + resultado);
+	else if (resultado < 100)
+	{
+	_putchar(' '), _putchar(' '), _putchar('0' + resultado / 10)
+		_putchar('0' + resultado % 10);
+	}
+	else
+	{
+	_putchar(' '), _putchar('0' + resultado / 100)
+	_putchar('0' + (resultado / 10) % 10), _putchar('0' + resultado % 10);
+	}
+	}
+	_putchar('\n');
+	}
 }
-
