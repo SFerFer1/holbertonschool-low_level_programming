86e667cfd0bcce826d18051f7cd7700295849531
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 11:48:56 +0000

    second try

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -23,7 +23,7 @@ void print_times_table(int n)
 	 if (multiplo == 0 )
 	 _putchar('0'); 
 	    else if (resultado < 10 )
-                _putchar(' '), _putchar(' '), _putchar('0' + resultado);
+        _putchar(' '),_putchar(' '), _putchar(' '), _putchar('0' + resultado);
             else if (resultado < 100)
                 _putchar(' '), _putchar('0' + resultado / 10), _putchar('0' + resultado % 10);
             else
