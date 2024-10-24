b52c1d9c9a687ea635f6442eed64729b237aa855
SFerFer1 <10048@holbertonstudents.com>
2024-10-24 14:15:29 +0000

    second try

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -8,14 +8,15 @@
 int main(void)
 {
 int i;
-long valoractual = 1;
-long valoranterior = 2;
-long cambiovalor;
+unsigned long valoractual = 1;
+unsigned long valoranterior = 2;
+unsigned long cambiovalor;
+
+	printf("%lld", valoractual);
 for (i = 0; i < 98 ; i++)
 {
 
-
-        printf(", ");
+	printf(", %lld", valoranterior);
 
 printf("%ld", valoractual);
 cambiovalor = valoractual + valoranterior;
