088b3d79c73f3f6c9f5c04bc372e676d5a77b8b7
SFerFer1 <10048@holbertonstudents.com>
2024-10-25 13:54:48 +0000

    friday

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -1,40 +1,39 @@
 #include <stdio.h>
 /**
-* main - combination of 2 digits
+*main - combination of 2 digits
 *
-*  Return: Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
 int i;
 int n;
-
 for (i = 0; i < 100; i++)
 {
 
 
 for (n = i + 1 ; n < 100; n++)
-	{i
+{
 	if (n != i)
 		{
-		putchar((i / 10) + '0'); 
+		putchar((i / 10) + '0');
 		putchar((i % 10) + '0');
-	   	
-	    putchar(' ');
+		
+		putchar(' ');
 
-	    	putchar((j / 10) + '0');
-		putchar((j % 10) + '0'); 
+		putchar((n / 10) + '0');
+		putchar((n % 10) + '0');
 		if (i != 8 || n != 9)
-			{
+		{
 			putchar (',');
 			putchar(' ');
 			}
 		}
-	}
+		}
 
-}
+		}	
 putchar('\n');
 return (0);
 
 
-}
+} 
