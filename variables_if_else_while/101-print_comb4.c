a24860c7f3988e27d5026e7ae5d089884a137cbe
SFerFer1 <10048@holbertonstudents.com>
2024-10-25 13:27:43 +0000

    friday

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -20,18 +20,17 @@ for (t = n + 1 ; t < 10; t++)
 	putchar (i + '0');
 	putchar (n + '0');
 	putchar (t + '0');
-	
 
-if (i != 7 || n != 8 || t != 9 )
+if (i != 7 || n != 8 || t != 9)
 {
 	putchar (',');
-       	putchar(' ');
+	putchar(' ');
 }
 
 
 
 
-}                        
+}
 }
 }
 putchar('\n');
