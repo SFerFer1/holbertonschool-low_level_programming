60fb4a44a1f9f6c9b8a0d22453ef05b8b32a060a
SFerFer1 <10048@holbertonstudents.com>
2024-10-25 13:08:42 +0000

    friday

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+* main - combination of 3 digits
+*
+*  Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+int n;
+int t;
+for (i = 0; i < 10; i++)
+{
+for (n = i + 1 ; n < 10; n++)
+{
+
+for (t = n + 1 ; n < 10; n++)
+
+	if (n != i && i != t &&t != n )
+	{
+	putchar (i + '0');
+	putchar (n + '0');
+	putchar (t + '0');
+	}
+
+if (i != 9 || n != 8 || t != 7 )
+	putchar (',');
+       	putchar(' ');
+
+
+
+
+
+}                        
+}
+putchar('\n');
+return (0);
+
+
+}
