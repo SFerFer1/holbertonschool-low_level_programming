1b318ed5593ccbcc9046c301631ea13b182436f3
SFerFer1 <10048@holbertonstudents.com>
2024-10-28 16:42:04 +0000

    8 my try

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+*  print_square - print a square
+*@size: Tama√o del cuadrado
+*  Return: nothing
+*/
+void print_square(int size)
+{
+int i;
+
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0 ; i < size ; i++)
+{
+_putchar('#');
+for (i = 0 ; i < size ; i++)
+{
+_putchar('#');
+
+}
+}
+}
+}
