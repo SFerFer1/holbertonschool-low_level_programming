066c1f67ed35305b7457b54389c619c766f2901e
SFerFer1 <10048@holbertonstudents.com>
2024-10-28 12:33:43 +0000

    first try 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * _isupper - if upper case return 1
+ *
+ *@c: int c es un caracter cualquiera
+ *
+ *  Return:  1 if upper 0 otherwise
+ */
 int _isupper(int c)
 {
 if (c > 64  && c < 91)
