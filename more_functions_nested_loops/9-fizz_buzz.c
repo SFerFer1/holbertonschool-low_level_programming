c8fd6a9dd0e6e49c69ded13b2804a916a39b979c
SFerFer1 <10048@holbertonstudents.com>
2024-10-28 17:40:40 +0000

    8 my try

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -4,19 +4,32 @@
 *
 *  Return: nothing
 */
-void main(void)
+int main(void)
 {
 int i;
 
 for (i=0 ; i <= 100 ; i++)
 	{
 	if (i % 3 == 0)
+	{
 	printf("Fizz");
+	printf(" ");
+	}
 else if (i % 5 == 0)
+{
         printf("Buzz");
-else if (i % 3 == 0 && i % 5 == 0)
-printf(d%);
-
-
+	printf(" ");
 }
+else if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz");
+printf(" ");
+}
+else 
+{
+printf("%d" , i);
+printf(" "); 
+}
+}
+return (0);
 }
