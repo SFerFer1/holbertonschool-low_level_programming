b627304e7a5f7bc8a588f67f9f6c479746c4bc2c
SFerFer1 <10048@holbertonstudents.com>
2024-10-28 14:49:50 +0000

    first try 6

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*  print_diagonal - print \ n times
+*@n: int cualquier numero
+*  Return: nothing
+*/
+void print_diagonal(int n)
+{
+int i;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 0 ; i < n ; i++)
+_putchar('\\');
+_putchar('\n');
+}
+
+
+
+}
