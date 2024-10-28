e9aad4b9a4d157aab4e013a34c07531fd72a23b9
SFerFer1 <10048@holbertonstudents.com>
2024-10-28 13:22:29 +0000

    first try

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isdigit - see number between a and b
+ *
+ *@a: int a es un numero cualquiera
+ *@b: int b es un numero cualquiera
+ *
+ *  Return: always 0
+ */
+int mul(int a, int b)
+{
+
+int resultado;
+
+resultado = a * b;
+if (resultado == 4)
+{
+resultado = 4;	
+}
+return (0);
+}
