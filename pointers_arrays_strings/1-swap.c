81f6dc17e6d0e6064546ab6ddb56c50c67d1923d
SFerFer1 <10048@holbertonstudents.com>
2024-10-29 15:41:29 +0000

    second try 9

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,16 +1,14 @@
 #include "main.h"
 /**
-*  swap_int- swap values
-*@n:  pointer of a
-*@b:  pointer of b
-*  Return: nothing
+*swap_int- swap values
+*@a:pointer of a
+*@b:pointer of b
+*Return: nothing
 */
-
 void swap_int(int *a, int *b)
 {
 int c;
 c = *a;
-*a= *b;
+*a = *b;
 *b = c;
-
 }
