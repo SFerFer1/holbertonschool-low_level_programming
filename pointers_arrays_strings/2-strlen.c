efbb015492917b0ceba99577f5c3d21c5511f3c6
SFerFer1 <10048@holbertonstudents.com>
2024-10-29 15:57:04 +0000

    second try 9

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*  _strlen - retun legth of script
+*@s:a string
+*  Return: retun length of the string
+*/
+int _strlen(char *s)
+{
+int i = 0;
+
+while (*s[i] != 0)
+i++;
+
+return (i);
+}
---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*  _puts - prints a script
+*@str:a string
+*  Return: always 0
+*/
+void _puts(char *str)
+{
+
+
+
+
+
+}
