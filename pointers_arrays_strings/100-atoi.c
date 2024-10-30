d31b1cb7c93da37da19558862dae9df3557f4e90
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 15:56:11 +0000

    works

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,8 +8,33 @@
 */
 int _atoi(char *s)
 {
+int i = 0, j = 0;
+
+while (s[j] != '\0')
+{
+j++;
+}
+
+if (j = 0)
+{
+return (0);
+}
+else
+{
+while (s[i] == ' ') 
+{
+	i++;
+}
+if (s[i] == '-')
+{
+i++
+}
+}
 
 
 
+
+
+}
 
 }
