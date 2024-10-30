48df8c165f617e953b1b8c6115d3055d92e02c5d
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 16:14:20 +0000

    works

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-*  _atoi - copy string pointed
+*  _atoi - simulate aoti
 *@s:a char
 *
 *  Return: char the pointer to dest
@@ -8,7 +8,7 @@
 */
 int _atoi(char *s)
 {
-int i = 0, j = 0;
+int i = 0, j = 0, signo = 1;
 
 while (s[j] != '\0')
 {
@@ -26,14 +26,20 @@ while (s[i] == ' ')
 	i++;
 }
 if (s[i] == '-')
-{
+{}
+signo =-1;
 i++;
 }
 else if (s[i] == '+')
 {
+signo =+1;
 i++;
 }
-
+while (dest[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
 
 }
 return (0);
