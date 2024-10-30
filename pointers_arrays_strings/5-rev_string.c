ab99d5e947513d362c559a6488175965cffff438
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 12:48:45 +0000

    works

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,6 +8,7 @@
 void rev_string(char *s)
 {
 int i = 0, j = 0, n = 0, t = 0;
+char cambio;
 
 while (s[j] != '\0')
 {
@@ -21,7 +22,11 @@ for (i = j-1 ; i >= 0 ; i--)
 {
 if(t ==0)
 {
+
+cambio = s[i]; 
 s[i] = s[n];
+s[n] = cambio;
+
 t++;
 }
 }
