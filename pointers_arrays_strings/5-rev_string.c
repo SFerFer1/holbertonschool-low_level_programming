b800a68c4f52a3d6b5ffb45dd392b5122df367ed
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 12:36:11 +0000

    works

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,28 @@
 */
 void rev_string(char *s)
 {
-int i = 0, j = 0, n = 0;
+int i = 0, j = 0, n = 0, t = 0;
 
 while (s[j] != '\0')
 {
 j++;
 }
 
-for (i = j ; i >= 0 ; i--)
-
 while (s[n] != '\0')
 {
-
+t=0
+for (i = j ; i >= 0 ; i--)
 {
-s[i] = s[n];
-
+if(t ==0)
+{
+s[n] = s[i];
+t++;
+}
 }
 n++;
+
 }
+
 _putchar('\n');
 }
 
