599bc0f4209fd818583469f621435052a911cda6
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 12:25:34 +0000

    works

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,33 @@
 #include "main.h"
 /**
-*  print_rev - prints a script in rev
+*  rev_string - change a script to rev
 *@s:a string
 *  Return: nothing
 *
 */
-void rev_string(char *s)
+void rev_string(char *s)}
+
+{
+int i = 0, j = 0, n = 0;
+
+while (s[j] != '\0')
+{
+j++;
+}
+
+
+while (s[n] != '\0')
+{
+
+
+
+for (i = j ; i >= 0 ; i--)
+{
+s[n] = s[i];
+
+}
+n++
+}
+_putchar('\n');
+}
+
