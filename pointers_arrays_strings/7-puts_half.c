9a46a2470d99d2da75fe1726c602e19fcefc5177
SFerFer1 <10048@holbertonstudents.com>
2024-10-30 13:44:21 +0000

    works

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*  puts_half - print half of a string
+*@str:a string
+*  Return: nothing
+*
+*/
+void puts_half(char *str)
+{
+int i = 0, j = 0;
+
+while (s[j] != '\0')
+{
+j++;
+}
+j--;
+j = j / 2
+
+for (i = j ; i >= 0 ; i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+
+}
