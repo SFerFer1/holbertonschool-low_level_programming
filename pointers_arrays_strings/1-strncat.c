65ce77bc910ba30eedab26e26c783d383d01cc09
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 11:54:26 +0000

    run of 10

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*  *_strncat - combine n caracters
+*@dest: a string
+*@src: a string
+*@n:a int
+*  Return: a char
+*
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+char *ret = dest;
+int i = 0;
+
+while (*dest != '\0')
+{
+dest++;
+}
+
+while (i < n)
+{
+*dest = *src;
+dest++;
+src++;
+n++;
+}
+
+
+
+
+}
