930190f3ad84afcdfef381671b7b9b7ec2dda060
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 15:02:16 +0000

    run of 10

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,8 +1,23 @@
 #include "main.h"
 /**
-* reverse_array - copy string pointed
-*@a:a int
-*@n:a int
-*  Return: void
+* *string_toupper - copy string pointed
+*@n:a char
+*Return: void
 *
 */
+char *string_toupper(char *n)
+{
+int i = 0; 
+int countn = 0;
+
+while (*n != '\0')
+{
+countn++;
+}
+for ( i = 0 ; i < countn ; i++)
+{
+if(n[i] > 96 && n[i] < 123 )
+}
+n[i] =(n[i]-32)
+return (n)
+}
