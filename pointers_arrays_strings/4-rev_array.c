18305b584ea5b1746095de854c5db4e8affb34d4
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 14:14:20 +0000

    run of 10

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -9,13 +9,13 @@
 void reverse_array(int *a, int n)
 {
 int  t = 0 ;
-int temp = 0;
+
 
 while (t < (n-1))
 {
-temp = a[t];
+ int temp = a[t];
 a[t] = a[(n-1)];
-
+a[(n-1)] = temp;
 }
 
 
---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+* reverse_array - copy string pointed
+*@a:a int
+*@n:a int
+*  Return: void
+*
+*/
