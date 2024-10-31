002aafa682e456491c232a63c9e2690ac1feefb3
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 14:26:07 +0000

    run of 10

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,14 @@
 */
 void reverse_array(int *a, int n)
 {
-int  t = 0 ;
+int  t = 0;
 
 
 while (t < n / 2)
 {
- int temp = a[t];
-a[t] = a[((n-1)-t)];
-a[((n-1)-t)] = temp;
+int temp = a[t];
+a[t] = a[((n - 1) - t)];
+a[((n - 1) - t)] = temp;
 t++;
 }
 
