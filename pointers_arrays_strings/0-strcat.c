7f65461232eececc3a6eb4399be29a2e541eef74
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 11:39:17 +0000

    run of 10

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*  *_strcat - copy string pointed
+*@dest:a char
+*@src:a char
+*  Return: char the pointer to dest
+*
+*/
+char *_strcat(char *dest, char *src)
+{
+while (*dest != '\0') 
+{
+dest++;
+}
+
+while (*src != '\0')
+{
+*dest = *src;
+dest++;
+src++;
+}
+
+*dest = '\0';
+}
