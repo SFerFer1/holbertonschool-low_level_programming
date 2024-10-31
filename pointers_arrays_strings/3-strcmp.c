409ab250b9aa3547f648639bf1e6a9f7103ff019
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 12:58:40 +0000

    run of 10

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+* _strcmp - copy string pointed
+*@s1:a char
+*@s2:a char
+*  Return: a int
+*
+*/
+int _strcmp(char *s1, char *s2)
+{
+int tamaÃo1 = 0 ,tamaÃ±Ão2 =;
+
+while (*s1 != '\0')
+{
+s1++;
+tamaÃo1++;
+}
+while (*s2 != '\0')
+{
+s2++;
+tamaÃ±o++;
+}
+s1 = 0;
+s2 = 0;
+
+if( s1 > s2 )
+{
+return (1);
+}
+else if (s2 > s1)
+{
+return (-1);
+}
+else
+{
+return (0);
+}
+}
