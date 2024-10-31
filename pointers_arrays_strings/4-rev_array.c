59eb89214d0772a7b30dda158390ae5537fbfc2f
SFerFer1 <10048@holbertonstudents.com>
2024-10-31 13:45:32 +0000

    run of 10

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* reverse_array - copy string pointed
+*@a:a int
+*@n:a int
+*  Return: void
+*
+*/
+void reverse_array(int *a, int n)
+{
+int i , n = 0;
+
+for (i = n ; i > 0 ; i--)
+{
+a[n] = a [i]
+n++
+}
+
+
+}
