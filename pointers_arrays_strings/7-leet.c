24818eeec77824ad219a7060ae8b0ba03c2aff2b
SFerFer1 <10048@holbertonstudents.com>
2024-11-01 12:16:45 +0000

    Hot fix

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -11,9 +11,9 @@ int i = 0;
 int j = 0;
 char numbers[] = "4433007711";
 char words[] = "aeotlaAEOTLA";   
-for (int i = 0; n[i] != '\0'; i++)
+for (i = 0; n[i] != '\0'; i++)
 {
-for (int j = 0; j < 10; j++)
+for (j = 0; j < 10; j++)
 {
 if (n[i] == words[j])
 {
