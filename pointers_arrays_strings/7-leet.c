6663ae2a55dddf141ac36f79279f6cda65cf4faa
SFerFer1 <10048@holbertonstudents.com>
2024-11-01 12:00:06 +0000

    Hot fix

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -7,6 +7,20 @@
 */
 char *leet(char *n)
 {
+int i = 0;
+int j = 0;
+char array[] = {'a', 'e', 'o', 't', 'l'};
 
-
+while (n[i] != '\0')
+{
+for (j = 0; array[j] != '\0'; j++)
+{
+if (n[i] == array[j])
+{
+array[j] -= 32;
+}
+}
+i++;
+}
+return (n);
 }
