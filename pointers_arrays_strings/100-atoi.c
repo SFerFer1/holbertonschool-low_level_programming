1dc118ff8242948931cf8fe1cffabe6ae4e217a6
SFerFer1 <10048@holbertonstudents.com>
2024-11-01 15:22:29 +0000

    Hot fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -54,12 +54,8 @@ while (s[i] >= '0' && s[i] <= '9')
 {
 if (num > (INT_MAX - (s[i] - '0')) / 10)
 {
-return (sign == 1) ? INT_MAX : INT_MIN;
+return ((sign == 1) ? INT_MAX : INT_MIN);
 }
-
-
-
-
 num = num * 10 + (s[i] - '0');
 i++;
 }
