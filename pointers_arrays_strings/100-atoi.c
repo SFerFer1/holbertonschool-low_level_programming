9bf4d44b42ab8fbfaa3540bdb9b514029bd198eb
SFerFer1 <10048@holbertonstudents.com>
2024-11-01 15:15:53 +0000

    Hot fix

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -50,9 +50,6 @@ i++;
 
 while (s[i] >= '0' && s[i] <= '9')/*transformar de caracter a numaro*/
 {
-if (num > (INT_MAX - (s[i] - '0')) / 10)
-return (sign == 1) ? INT_MAX : INT_MIN;
-
 num = num * 10 + (s[i] - '0');
 i++;
 }
