6fff3c59a7462ed17c3bc659eb5abffae82f8b2b
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 13:46:38 +0000

    new week }

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -8,28 +8,25 @@
 */
 unsigned int _strspn(char *s, char *accept)
 {
-int i = 0;
-int n = 0;
+int i = 0 , n = 0 ,e = 0;
 unsigned int t;
-int e = 0;
-while (accept[i] == '\0')
-{
-n = 0;
-e = 0;
 
-while (s[e] == '\0')
+for (i = 0; s[i] != '\0'; i++)
 {
-if (s[e] == accept[i])
+for (n = 0; accept[n] != '\0'; n++)
 {
-n++;
-}
-if (n == 1)
+if (s[i] == accept[n])
 {
-t++;
+t = 1;
+break;
 }
-e++;
 }
-i++;
+if (!t) 
+{
+break;
+}
+
+t++
 }
 return (t);
 }
