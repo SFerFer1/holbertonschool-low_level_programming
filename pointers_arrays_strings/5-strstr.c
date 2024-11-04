c7e3cc12d245dbbcb4e4ce93d9a03ecdf8566b39
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 14:23:31 +0000

    new week }

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include <stddef.h>
+#include "main.h"
+/**
+*  *_strstr - return number bytes ignitial segment
+*@haystack:a  char
+*@needle: a char
+*Return: a char
+*
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0, n = 0;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+
+for (n = 0; needle[n] != '\0'; n++)
+{
+if (haystack[i] == needle[n])
+{
+return (&s[i]);
+}
+}
+}
+
+return (NULL);
+
+}
