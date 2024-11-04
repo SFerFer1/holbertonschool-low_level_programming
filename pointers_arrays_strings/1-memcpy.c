4aa009347f66bc3aebe5f4f356e01447d5a24ffa
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 12:25:32 +0000

    new week

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*  *_memcpy - print n of a string
+*@dest:a  char
+*@src:a char
+*@n: a int
+*Return: a char
+*
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+
+
+return (&dest);
+}
---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#define NULL ((void *)0)
+#include "main.h"
+/**
+* *_strchr - print n of a string
+*@s:a  char
+*@c:a char
+*Return: a char
+*
+*/
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+	if (s[i] == c)
+	{
+		return (&s[i]);
+	}
+i++;
+}
+return (NULL);
+}
