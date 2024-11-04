f6cb9e3d35b928251f7c6c2354d173a80974f939
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 14:08:39 +0000

    new week }

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -8,6 +8,26 @@
 */
 char *_strpbrk(char *s, char *accept)
 {
+int i = 0, n = 0;
+ int t = 0;
 
+for (i = 0; s[i] != '\0'; i++)
+{
+
+for (n = 0; accept[n] != '\0'; n++)
+{
+if (s[i] == accept[n])
+{
+t = s[i];
+break;
+}
+}
 
 }
+if (t != 0)
+{
+return (NULL);
+}
+return (t);
+}
+
