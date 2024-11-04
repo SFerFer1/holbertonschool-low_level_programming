49930115ff35532243d83407a97eeaf4fb3d782a
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 13:26:28 +0000

    new week

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -12,12 +12,12 @@ int i = 0;
 int n = 0;
 unsigned int t;
 int e = 0;
-while (accept[i] == '\0')
+while (s[i] == '\0')
 {
 n = 0;
-while (s[e] == '\0')
+while (accept[e] == '\0')
 {
-
+if (s[i] == accept[e])
 if (n == 1)
 {
 t++;
