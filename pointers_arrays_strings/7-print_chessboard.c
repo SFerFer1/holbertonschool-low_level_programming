af4c4b7676cf07d39c272918b9cbac02a7f4b374
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 15:49:38 +0000

    new week }

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,15 @@
+#include <stddef.h>
+#include "main.h"
+/**
+*  print_chessboard - make a chess board
+*@a:a char
+*Return: a char
+*
+*/
+void print_chessboard(char ch (*a)[8])
+{
+printf("%d", ch[0][2]);
+
+
+
+}
