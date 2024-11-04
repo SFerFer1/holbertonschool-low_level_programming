babf4dfac252c374e15405bbede40dd71186a182
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 14:52:28 +0000

    new week }

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,44 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, n = 0;
+int i = 0, n = 0, nlargo = 0,contador = 0,contador2 = 0,verificador = 0;
+
+for (n = 0; needle[n] != '\0'; n++)
+{
+nlargo++;
+}
+
+
 
 for (i = 0; haystack[i] != '\0'; i++)
 {
 
 for (n = 0; needle[n] != '\0'; n++)
 {
+
+while (contador != nlargo)
+{
 if (haystack[i] == needle[n])
 {
+contador2++;
+
+}
+{
+if (haystack[i] == ' ' || haystack[i] == ', ')
+{
+contador = 0
+}
+contador++
+}
+
+{
+if (contador2 == nlargo)
 return (&haystack[i]);
+
+}
 }
 }
 }
 
 return (NULL);
-
 }
