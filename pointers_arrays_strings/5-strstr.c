a3ce17ebdd35a5df948975be03b2d9ab47658055
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 15:09:55 +0000

    new week }

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,39 +9,18 @@
 */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, n = 0, nlargo = 0,contador = 0,contador2 = 0;
-
-for (n = 0; needle[n] != '\0'; n++)
+int i = 0, n = 0;
 {
-nlargo++;
-}
 for (i = 0; haystack[i] != '\0'; i++)
 {
-
-for (n = 0; needle[n] != '\0'; n++)
+while (haystack[i + n] == needle[n] && needle[n] != '\0')
 {
-
-while (contador != nlargo)
+n++;
+}
+if (needle[n] == '\0')
 {
-if (haystack[i] == needle[n])
-{
-contador2++;
-
-}
-
-if (haystack[i] == ' ' || haystack[i] == ',')
-{
-contador = 0;
-}
-contador++;
-
-if (contador2 == nlargo)
-{
-return (&haystack[i]);
+return &haystack[i];
 }
 }
-}
-}
-
-return (NULL);
+return NULL;
 }
