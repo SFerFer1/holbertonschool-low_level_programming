44c92e2913e11ebfce487209c410ed36f50bf158
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 16:23:23 +0000

    new week }

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,16 @@
+#include <stddef.h>
+#include "main.h"
+/**
+* print_diagsums - make a chess board
+*@a:a char
+*@size: rhe size of table
+*Return: void
+*
+*/
+void print_diagsums(int *a, int size)
+{
+
+int square[size][size];
+
+
+}
