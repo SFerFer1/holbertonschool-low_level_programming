b06a9e4d0f6552e56de3633f4c950e39dbb0f89f
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 16:44:47 +0000

    new week }

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include "main.h"
+/**
+*  _puts_recursion - simulate puts
+*@s:a char
+*  Return: void
+*
+*/
+void _puts_recursion(char *s)
+{
+int i = 0;
+int n = 0;
+while(i != '\0')
+{
+n++;
+i++;
+}
+
+for(i = 0 ; i < n ; i++)
+{
+_putchar(s[i]);
+}
+
+}
