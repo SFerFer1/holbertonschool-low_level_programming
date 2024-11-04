6bc646a7dcd9cf826e383a456b4c8b4532b2194e
SFerFer1 <10048@holbertonstudents.com>
2024-11-04 11:32:45 +0000

    new week

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+
+#include "main.h"
+/**
+*  *_memset - print n of a string
+*@s:a  char
+*@b:a char
+*@n: a int
+*Return: a char
+*
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+int i;
+for ( i = 0 ; i  < n ; i++)
+{
+
+s[i] = "b"
+
+
+}
+return (&s);
+}
