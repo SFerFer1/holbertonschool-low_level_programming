b9875b67a40bf755a414e59f5ec254ffa5d2ec0f
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 17:11:29 +0000

    try 2

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
 *main - return name 
-*@n:an int
+*@argv: a string of string 
 *Return: int
 *
 */
-int main (char *argv[])
+int main (int argc,char *argv[])
 {
 int i = 0;
 
+(void)argc;
 for ( i = 0; argv[0][i] != '\0'; i++)
 {
 _putchar(argv[0][i]);
