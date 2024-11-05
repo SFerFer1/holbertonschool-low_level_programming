435a9b139438cb5cf1b45bcd777e656f7fb8b666
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 17:17:32 +0000

    try 2

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*main - return number
+*@argv: a string of string
+*@argc: a int
+*Return: int
+*
+*/
+int main(int argc, char *argv[])
+{
+
+(void)argv;
+_putchar(argc);
+
+_putchar('\n');
+return (0);
+}
