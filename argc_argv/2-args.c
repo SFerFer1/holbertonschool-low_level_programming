61981b3d05bb129733967073f7d3b914769819f2
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 17:29:38 +0000

    try 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*main - return number
+*@argv: a string of string
+*@argc: a int
+*Return: int
+*
+*/
+int main(int argc, char *argv[])
+{
+int i = 0;
+int n = 0;
+
+
+while(n < argc)
+{
+for (i = 0; argv[n][i] != '\0'; i++)
+{
+_putchar(argv[n][i]);
+}
+_putchar('\n');
+n++;
+}
+return (0);
+}
