232c05dca9060949e2af1e6c70cf15acaa49b762
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 17:42:00 +0000

    try 2

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+*main - return number
+*@argv: a string of string
+*@argc: a int
+*Return: int
+*
+*/
+int main(int argc, char *argv[] int a ,int b)
+{
+int resultado;
+char error["Error"];
+(void)argv
+if ( argc > 3)
+{
+for (i = 0; error[i] != '\0'; i++)
+{
+_putchar(error[i]);
+}
+}
+else
+{
+resultado = a *b;
+_putchar('resultado');
+_putchar('\n');
+}
+return (0)
+}
