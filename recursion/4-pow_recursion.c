89d2b50bf2e4caf63dfee8f5f521f39a4589e55e
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 13:01:22 +0000

    tring recurcion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+*  _pow_recursion - factorial
+*@x:an int
+*@y: an int
+*  Return: int
+*
+*/
+int _pow_recursion(int x, int y)
+{
+if ( y < x )
+{
+return (-1)
+}
+else
+{
+while (factorial(y - 1) > 0)
+{
+
+x =  x *_pow_recursion(x)
+}
+
+}
+
+}
+
