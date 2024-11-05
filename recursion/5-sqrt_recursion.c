b1c10eb7aabd0d9f1f8ba4f2d46bb092a5d5f83d
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 13:43:05 +0000

    tring recurcion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*  _sqrt_recursion - factorial
+*@n:an int
+*  Return: int
+*
+*/
+int _sqrt_recursion(int n)
+{
+int raiz;
+
+if (raiz == n)
+{
+return(raiz)
+}
+if (raiz > n)
+{
+return (-1)
+}
+raiz = _sqrt_recursion(n);
+}
+
