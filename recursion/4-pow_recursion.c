3e4b38a85fd97592a178e7325d1fd57860d1fb65
SFerFer1 <10048@holbertonstudents.com>
2024-11-05 13:25:24 +0000

    tring recurcion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,22 +8,22 @@
 */
 int _pow_recursion(int x, int y)
 {
-if ( y < 0 )
+if (y < 0)
 {
 return (-1);
 }
-if ( y = 0 )
+if (y == 0)
 {
 return (1);
 }
 
 
-if ( y == 1)
+if (y == 1)
 {
 return (x);
 }
 
-x =x *_pow_recursion(x, y - 1);
+x = x *_pow_recursion(x, y - 1);
 
 
 
