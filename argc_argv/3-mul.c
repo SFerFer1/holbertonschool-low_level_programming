67e3ac18ed32b5287445c10ed71da6e02b854007
SFerFer1 <10048@holbertonstudents.com>
2024-11-06 11:38:58 +0000

    probar otro dia

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdio.h>
 /**
 *main - return number
 *@argv: a string of string
@@ -7,10 +8,10 @@
 *Return: int
 *
 */
-int main( int argc )
+int main(int argc, char *argv[])
 {
 int resultado;
-char error["Error\n"];
+char error[7]"Error\n";
 int i;
 int a = atoi(argv[1]);
 int b = atoi(argv[2]);
