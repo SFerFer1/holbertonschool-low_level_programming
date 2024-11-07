33157b1953780720f2c1ec8d24c6973390f9b931
SFerFer1 <10048@holbertonstudents.com>
2024-11-07 12:11:35 +0000

    thursday

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -15,9 +15,9 @@ if (n == 0 || n == 1 || n < 0)
 return (0);
 }
 
-if (numero *numero == n)
+if ((n/numero)%2 == 0 )
 {
-printf("hola");
+
 return (1);
 }
 else if (numero *numero> n)
@@ -31,9 +31,9 @@ return (helper(n, numero + 1));
  * is_prime_number - setea numero a 2.
  * @n: El número al cual sesabra si es primo.
  *
- * Return: 1 0 sero segun si es primo o no-.
+ * Return: 1 zero segun si es primo o no-.
  */
 int is_prime_number(int n)
 {
 return helper(n,2);
-}
+i}
