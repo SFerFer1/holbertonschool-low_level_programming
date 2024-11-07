70759d0bfdbdefbc4d560f0b796fe4195453f172
SFerFer1 <10048@holbertonstudents.com>
2024-11-07 11:34:00 +0000

    thursday

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,47 +1,38 @@
 #include "main.h"
 #include <stdio.h>
 /*
-*  _sqrt_helper- factorial
+*_sqrt_helper - hace el resto
 *@n:an int
 *@raiz: an int
 *  Return: int
 *
 */
-int _sqrt_helper(int n,int raiz )
+int _sqrt_helper(int n, int raiz)
 {
-/*
-if (n < 0)
-{
-return -1;
-}
-*/
 
 if (n == 0 || n == 1)
 {
-return n;
+return (n);
 }
 
 if (raiz * raiz == n)
 {
-return raiz;
+return (raiz);
 }
 else if (raiz * raiz > n)
 {
-return -1;
-} 
-return _sqrt_helper(n, raiz + 1);
+return (-1);
+}
+return (_sqrt_helper(n, raiz + 1));
 }
 /*
-*  _sqrt_recursion - factorial
+*_sqrt_recursion - le da a helper el valor 0
 *@n:an int
-*  Return: int
+*
+*Return: int
 *
 */
 int _sqrt_recursion(int n)
 {
-    /*if (n < 0)
-    {
-        return -1;
-    }*/
-    return _sqrt_helper(n, 0);
+return (_sqrt_helper(n, 0));
 }
