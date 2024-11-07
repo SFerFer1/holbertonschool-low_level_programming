d37eaebb2099e43d5e92c6ba2385d73c338dd9fd
SFerFer1 <10048@holbertonstudents.com>
2024-11-07 11:43:44 +0000

    thursday

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number - Encuentra la raíz cuadrada de un número de forma recursiva.
+ * @n: El número al cual se le calculará la raíz cuadrada.
+ * @raiz: El valor que se utiliza para comprobar si es la raíz cuadrada de n.
+ *
+ * Return: La raíz cuadrada de n si la encuentra, o -.
+ */
+int is_prime_number(int n)
+{
+
+
+
+
+
+}
