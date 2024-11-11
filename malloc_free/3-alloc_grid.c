a5e2b6d648e6d04dcf411a6841fe8b20735b1d4a
SFerFer1 <10048@holbertonstudents.com>
2024-11-11 14:35:37 +0000

    cambiar tamanio 1

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+ * alloc_grid -returns 2 dimencional array
+ *
+ *@width: a int
+ *@height: a int
+ *
+ *Return: null f str = NULL
+ */
+int **alloc_grid(int width, int height)
+{
+int **figura;
+int i;
+int n;
+
+if (width <= 0 || height <= 0)
+return (NULL);
+
+figura = malloc(height * sizeof(int *));
+if (figura == NULL)
+return (NULL);
+
+
+for (i = 0; i < height; i++)
+{
+figura[i] = malloc(width * sizeof(int));
+}
+for (i = 0; i < height; i++)
+{
+for (n = 0; n < width; n++)
+{
+figura[i][n] = 0;
+}
+}
+return (figura);
+}
