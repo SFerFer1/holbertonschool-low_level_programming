8d071be34955b983e22c88e21c9d09523585f351
SFerFer1 <10048@holbertonstudents.com>
2024-11-11 12:52:07 +0000

    cambiar tamanio 1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,20 +1,24 @@
-
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
 /**
 * create_array - crate array of char
-* 
-*@size: size of the array 
+*
+*@size: size of the array
 *@c: un char cualquier
 *
-*  Return:  0 
+*  Return:  0
 */
 char *create_array(unsigned int size, char c)
 {
 
+char *b;
+
 if (size == 0)
-return (0);
+return (NULL);
 
-c = malloc(sizeof(*c) * size);
+b = malloc(sizeof(c) * size);
 
-free(c);
+free(b);
+return (b);
 }
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strdup -returns a pointer to a newly allocated space in memory
+ * 
+ *@str: a string
+ *
+ *  Return:  null f str = NULL
+ */
+char *_strdup(char *str)
+{
+char *t;
+
+if (str = NULL)
+return (NULL);
+
+t = malloc(sizeof(*str) * 3);
+
+
+retun(*t);
+}
