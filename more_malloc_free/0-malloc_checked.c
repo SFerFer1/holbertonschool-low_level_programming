d53029abe654bf97aece6ab8124c76e65ed35732
SFerFer1 <10048@holbertonstudents.com>
2024-11-11 15:59:12 +0000

    cambiar tamanio 2

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+* malloc_checked -returns 2 dimencional array
+*
+*@b: a int
+*
+*Return: null f str = NULL
+*/
+void *malloc_checked(unsigned int b)
+{
+
+
+
+
+}
