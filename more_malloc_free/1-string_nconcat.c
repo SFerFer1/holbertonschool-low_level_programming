c61f4f977fa884ded51ec33ace27a91cd2931458
SFerFer1 <10048@holbertonstudents.com>
2024-11-11 17:49:46 +0000

    cambiar tamanio 2

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -10,9 +10,47 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+int largo = 0;
+int largo2 = 0;
+int i;
+int t;
+char *resultado;
 
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[largo] != '\0')
+largo++;
 
+while (s2[largo2] != '\0')
+largo2++;
 
+resultado  = malloc((largo + largo2) +1);
+
+if (resultado == NULL)
+return (NULL);
+
+for (i = 0; i < largo; i++)
+{
+resultado[i] = s1[i];
+}
+if (n >= largo2)
+{
+for (n = 0; t < largo2; i++, t++)
+{
+resultado[i] = s2[t];
+}
+resultado[i] = '\0';
+}
+else 
+for (n = 0; t < n; i++, t++)
+{
+resultado[i] = s2[t];
+}
+
+return (resultado);
+}
 
 
 }
