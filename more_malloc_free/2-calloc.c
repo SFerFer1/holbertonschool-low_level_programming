c3f7e4670ed098615a29309155c18b5308fbb5d2
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 12:25:17 +0000

    martes repaso con checker

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+* _calloc - concate nate 1 strina and n chars of a second
+*
+*@nmemb: string 
+*@size: string 2
+*
+*Return: if dont work NULL
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *ret;
+unsigned int i;
+
+if (nmemb = 0 || size = 0)
+return (NULL);
+
+ret = malloc((nmemb *size)+1);
+
+for (i = 0 ; i < nmem; i++)
+ret[i] = 0
+
+return (ret);
+}
