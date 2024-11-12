2afb80b5aa5deee8583234cd5b65400bb6d12965
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 15:50:02 +0000

    martes repaso con checker

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,17 +2,17 @@
 #include<stdlib.h>
 /**
  * init_dog - say if a random number is positive or negative
- * 
+ *
  *@d: int n es un numero cualquiera
  *@name: int n es un numero cualquiera
  *@age: int n es un numero cualquiera
  *@owner: int n es un numero cualquiera
- *  Return:  0 
+ *  Return:  0
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
-exit (OK)
+exit("OK");
 
 
 d->name = name;
