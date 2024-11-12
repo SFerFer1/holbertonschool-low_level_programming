cf38cf5378c101f2ae82771c2eaaea97a25c9739
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 12:33:25 +0000

    martes repaso con checker

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -4,8 +4,8 @@
 /**
 * _calloc - concate nate 1 strina and n chars of a second
 *
-*@nmemb: string 
-*@size: string 2
+*@nmemb: string
+*@size: stringi 2
 *
 *Return: if dont work NULL
 */
@@ -17,9 +17,12 @@ unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
 
-ret = malloc(nmemb *size);
+ret = malloc(nmemb * size);
+if (ret == NULL)
 
-for (i = 0 ; i < (nmemb *size); i++)
+return (NULL);
+
+for (i = 0 ; i < (nmemb * size); i++)
 ret[i] = 0;
 
 return (ret);
