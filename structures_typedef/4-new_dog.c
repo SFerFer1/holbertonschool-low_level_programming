2b56f79788ddb860c5ec606aa0274490eb84dc4b
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 17:03:35 +0000

    martes repaso con checker

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,7 +13,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *dog1;
 
-
 if (name == NULL || owner == NULL)
 {
 return (NULL);
@@ -22,14 +21,25 @@ dog1 = malloc(sizeof(dog_t));
 
 if (dog1 == NULL)
 {
+free(dog1);
+return (NULL);
+}
+dog1->name = name;
+dog1->name = malloc(strlen(name) + 1);
+if (dog1->name == NULL)
+{
 free(dog1->name);
+free(dog1);
+return (NULL);
+}
+dog1->age = age;
+dog1->owner = owner;
+dog1->owner = malloc(strlen(owner) + 1);
+if (dog1->owner == NULL)
+{
 free(dog1->owner);
 free(dog1);
 return (NULL);
 }
-
-dog1->name = name;
-dog1->age = age;
-dog1->owner = owner;
 return (dog1);
 }
