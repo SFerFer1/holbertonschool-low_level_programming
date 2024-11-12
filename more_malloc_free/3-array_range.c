bdd26f2d6b9e0b53fb2618bb0a43ff62dcd1a125
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 12:50:40 +0000

    martes repaso con checker

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+* array_range - concate nate 1 strina and n chars of a second
+*
+*@min: string
+*@max: stringi 2
+*
+*Return: if dont work NULL
+*/
+int *array_range(int min, int max)
+{
+int size (max - min + 1);
+int ret[size];
+int *space;
+int i;
+
+if (min > max)
+return (NULL);
+
+space = maloc(size + 1);
+if (space == NULL)
+return (NULL);
+
+for (i = 0; min <= max ; min++ , i++)
+{
+ret[i] = min;
+
+}
+
+return (ret);
+}
