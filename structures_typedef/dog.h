04d40924262b65fa64443f991c24a188e7a5f09a
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 15:32:43 +0000

    martes repaso con checker

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * dog- is a dog 
+ * dog- is a dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
  *
+ * Description: i dont know
  */
 struct dog
 {
