04ad75b0374bf531b0b00b7b83a2239486978cf9
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 16:16:43 +0000

    martes repaso con checker

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -18,7 +18,7 @@ if (d->name == NULL)
 printf("Name: (nil)");
 else
 printf("Name: %s\n", d->name);
-printf("Age: %.2f\n", d->age);
+printf("Age: %f\n", d->age);
 if (d->owner == NULL)
 printf("Owner: (nil)");
 else
