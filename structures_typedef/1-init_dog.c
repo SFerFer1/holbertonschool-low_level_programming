e88e88faf1bdbc3c1f1a2f8ffd1b45d2f37d6396
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 15:51:46 +0000

    martes repaso con checker

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -12,9 +12,10 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d == NULL)
-exit("OK");
-
-
+{
+printf("OK\n");
+exit(0);
+}
 d->name = name;
 d->age = age;
 d->owner = owner;
