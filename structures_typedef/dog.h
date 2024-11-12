481f841f2f7033ec71e523ab5e39d32aa69c6226
SFerFer1 <10048@holbertonstudents.com>
2024-11-12 14:07:22 +0000

    martes repaso con checker

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,8 +1,12 @@
 
 #include <stdio.h>
-
-
-
+/**
+ * dog- is a dog 
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
+ *
+ */
 struct dog
 {
 char *name;
@@ -22,7 +26,7 @@ struct dog dog1 = {"Blacky", 3.5, "Mark"};
 
 printf("%s",dog1.name);
 printf("%f",dog1.age);
-printf("%d",dog1.owner);
+printf("%s",dog1.owner);
 return (1);
 }
 
