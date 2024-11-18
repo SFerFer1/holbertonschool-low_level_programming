2fcfa312f3efb3af6fabeb40b27f8eaf273a79dc
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 17:06:45 +0000

    Hot fix

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,34 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_numbers -returns a pointer to a newly allocated space in memory
+ *
+ *@n: a string
+ *@separator: the string
+ *
+ *Return: null f str = NULL
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+unsigned int resultado = 0;
+va_list args;
+
+if (separator == NULL)
+exit (NULL);
+
+va_start(args, separator);
+
+for (i = 0; i < n; i++)
+{
+if (separator != NULL && i > 0)
+{
+printf("%s", separator);
+}
+printf("%d", va_arg(args, int));
+}
+
+va_end(args);
+
+printf("\n");
+}
--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 int _putchar (char c);
-
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif /* MAIN_H */
