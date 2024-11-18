287643480f0ca501d381df394344f2ac175f7132
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 16:01:36 +0000

    pointer function 0

---
+++ function_pointers/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdarg.h>
+/**
+ * sum_them_all -returns a pointer to a newly allocated space in memory
+ *
+ *@n: a string
+ *
+ *Return: null f str = NULL
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+int i;
+if (n == 0)
+return (0);
+
+int resultado = 0;
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+resultado += va_arg(args, int);
+
+va_end(args);
+
+return (resultado);
+}
