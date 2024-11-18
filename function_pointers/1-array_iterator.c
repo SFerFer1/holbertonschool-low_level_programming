58f26af6867854fdea2551f8799bd139de1fa7c2
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 14:09:23 +0000

    pointer function 0

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * array_iterator -returns a pointer to a newly allocated space in memory
+ *
+ *@array: a string
+ *@action: a funtion
+ *@size: the size of array
+ *
+ *Return: null f str = NULL
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+int i;
+
+for (i = 0; i < size; i++)
+action(array[i]);
+
+
+
+
+
+}
