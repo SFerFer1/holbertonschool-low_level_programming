f0dc8c8c88d22630d2123e1026aecb70d3599637
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 17:37:55 +0000

    Hot fix

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * print_strings -returns a pointer to a newly allocated space in memory
+ *
+ *@n: a string
+ *@separator: the string
+ *
+ *Return: null f str = NULL
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+if ( i > 0)
+{
+if (separator == NULL)
+{
+printf('nil');
+}
+{
+printf("%s", separator);
+}
+printf("%d", va_arg(args, char));
+}
+
+va_end(args);
+
+printf("\n");
+
+}
+}
