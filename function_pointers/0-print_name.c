73832f6caff780d5fcd0467ac8723b6bc3fdbea3
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 13:46:05 +0000

    pointer function 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
 #include "function_pointers.h"
 /**
- * print_name - prints a name 
+ * print_name - prints a name
  *
  *@name: a int
  *@f: a int
@@ -12,12 +12,10 @@ void print_name(char *name, void (*f)(char *))
 int i = 0;
 int n = 0;
 
-while (n < '\0')
+while (n != '\0')
 n++;
 
 for (i = 0; i < n; i++)
 f(&name[i]);
-
-
 
 }
