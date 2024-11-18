ebbe8eaa89a343ca6102fe08248d290afc593a0c
SFerFer1 <10048@holbertonstudents.com>
2024-11-18 16:27:03 +0000

    Hot fix

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #include <stddef.h>
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 int _putchar (char c);
 
 
 int sum_them_all(const unsigned int n, ...);
-#endif /* MAIN_H */
+#endif
