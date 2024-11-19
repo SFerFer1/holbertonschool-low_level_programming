e584e9615a0aaf2f1720a68a80271028f660a11b
SFerFer1 <10048@holbertonstudents.com>
2024-11-19 13:09:05 +0000

    2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ va_list args;
 va_start(args, n);
 for (i = 0; i < n; i++)
 {
-if ( i > 0 && i != n)
+if ( i > 0 )
 {
 printf("%s", va_arg(args, char*));
 printf("%s", separator);
@@ -35,7 +35,7 @@ printf("%s", va_arg(args, char*));
 else
 {
 printf("%s", va_arg(args, char*));
-printf("%s", separator);
+
 }
 
 }
