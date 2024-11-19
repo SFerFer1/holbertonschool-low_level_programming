dbd929e4e5ba8d88a08cd5455fd6e6140f26fba6
SFerFer1 <10048@holbertonstudents.com>
2024-11-19 17:53:37 +0000

    2

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+/**
+ * int_index -returns a pointer to a newly allocated space in memory
+ *
+ *@s:operator
+ *@a:first number
+ *@b:second number
+ *
+ *Return: an int 
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+
+
+
+
+return (*op_add);
+
+
+
+}
