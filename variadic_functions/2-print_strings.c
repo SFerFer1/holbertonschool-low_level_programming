26ea8626700d108a290d7b675cb729a4f41a4f79
SFerFer1 <10048@holbertonstudents.com>
2024-11-19 13:45:14 +0000

    2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,14 +25,14 @@ for (i = 0; i < n; i++)
 printf("%s", va_arg(args, char*));
 }
 
-if (va_arg(args, char*) == NULL)
-{
-printf("(nil)");
-}
 else  if ( i < (n - 1))
 {
 printf("%s", va_arg(args, char*));
 printf("%s", separator);
+}
+else if (va_arg(args, char*) == NULL)
+{
+printf("(nil)");
 }
 else
 printf("%s", va_arg(args, char*));
