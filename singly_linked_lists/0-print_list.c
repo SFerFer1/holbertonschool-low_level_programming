c8fabb4c9627832d4566b132201a06fa53864f43
SFerFer1 <10048@holbertonstudents.com>
2024-12-02 14:37:41 +0000

    ej 0

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - prints all the elements of a list_t list.
+ * 
+ *@h: is the liat_t that reciv
+ *
+ *Return:
+ */
+size_t print_list(const list_t *h)
+{
+int cont = 0;
+const list_t *current = h;
+
+while (current != NULL)
+{
+
+if (current->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n",current->len, current->str);
+
+current = current->next;
+cont++;
+}
+
+return(cont);
+}
