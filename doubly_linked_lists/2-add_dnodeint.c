896511f25293202d2d39e1447538fc3b6e9c549d
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 12:17:20 +0000

    Hot fix

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new = malloc(sizeof(dlistint_t));
+
 if (new == NULL)
 return (NULL);
 
@@ -21,12 +22,12 @@ new->n = n;
 if (*head != NULL)
 {
 new->next = *head;
+(*head)->prev = new;
 }
 else
 new->next = NULL;
 
-if (*head != NULL)
-(*head)->prev = new;
+
 
 *head = new;
 
