9c96348fdb188b24e664aa162293cf58285fb15b
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 16:57:39 +0000

    Hot fix

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include <stddef.h>
+#include "main.h"
+/**
+* set_bit-reqa.
+*
+*@index: a binari number.
+*@n: a binari number.
+*
+* return: a int 
+*/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+if (index >= sizeof(unsigned long int) * 8)
+return (-1);
+
+*n = *n | (1UL << index);
+
+return (1);
+}
