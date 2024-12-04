d95b6e4b4210ab8ac1850da0cda80f920fcd918b
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 17:19:04 +0000

    Hot fix

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -8,7 +8,7 @@
 *
 * return: a int 
 */
-int set_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index);
 {
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
