47d2250f4b7c2f1b809f82ecf16eb2a81e6c1eb2
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 17:43:25 +0000

    Hot fix

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -1,17 +1,19 @@
 #include <stddef.h>
 #include "main.h"
 /**
-*get_bit-reqaasd.
+*clear_bit.
 *
 *@index: a binari number.
 *@n: a binari number.
 *
 * Return: 1 if the operation succeeded, or -1 if an error occurred.
 */
-int get_bit(unsigned long int n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 if (index >= sizeof(unsigned long int) * 8)
 return (-1);
 
-return ((n >> index) & 1);
+*n = *n & (1UL << index);
+
+return (1);
 }
