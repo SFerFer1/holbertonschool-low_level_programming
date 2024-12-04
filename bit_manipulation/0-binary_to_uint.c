dfd787f76358a74b5dcde560d7b9434185a107da
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 13:05:14 +0000

    Hot fix

---
+++ bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include "main.h"
+/**
+* binary_to_uint -returns 2 dimencional array
+*
+*@b: a binari number
+*
+*
+*Return:  yhe number in int
+*/
+unsigned int binary_to_uint(const char *b)
+{
+int i = 0;
+multiplicador = 1;
+unsigned int Total = 0;
+while (b[i] != '\0')
+{
+if (b[i] == 1)
+Total += multiplicador;
+
+multiplicador *=2;
+i++;
+}
+return (multiplicador);
+}
