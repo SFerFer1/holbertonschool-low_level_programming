4de9cca3a6537e77825dcdfcaa70028d56cb8830
SFerFer1 <10048@holbertonstudents.com>
2024-12-04 17:48:37 +0000

    Hot fix

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,11 @@
+#include <stddef.h>
+#include "main.h"
+/**
+*flip_bits-asdsasasa.
+*
+*@index: a binari number.
+*@n: a binari number.
+*
+* Return: 1 if the operation succeeded, or -1 if an error occurred.
+*/
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
