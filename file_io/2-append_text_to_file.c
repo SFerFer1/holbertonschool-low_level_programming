488f534d7c25c33cdc6f71c54d21009558a2d817
SFerFer1 <10048@holbertonstudents.com>
2024-12-05 17:38:04 +0000

    Funciona

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * append_text_to_file - check the code.
+ *
+ *@filename: Tamao del trianguo.
+ *@text_content: Tamao del trianguo.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+ssize_t bytes_written;
+
+if (filename == NULL)
+return (-1);
+
+fd = open(filename, O_CREAT | O_WRONLY, 0600);
+if (fd == -1)
+{
+return (-1);
+}
+
+if (text_content != NULL)
+{
+bytes_written = write(fd, text_content, strlen(text_content));
+if (bytes_written == -1)
+{
+close(fd);
+return (-1);
+}
+}
+close(fd);
+return (1);
+}
