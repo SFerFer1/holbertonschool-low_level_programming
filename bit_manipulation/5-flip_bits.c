bc2be217fb81f7e4e9d8a97131818deaa0ac443b
SFerFer1 <10048@holbertonstudents.com>
2024-12-05 12:30:25 +0000

    Hot fix

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -3,9 +3,20 @@
 /**
 *flip_bits-asdsasasa.
 *
-*@index: a binari number.
+*@m: a binari number.
 *@n: a binari number.
 *
 * Return: 1 if the operation succeeded, or -1 if an error occurred.
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
+
+while (xor_result)
+{
+count += xor_result & 1;
+xor_result >>= 1;
+}
+return (count);
+}
