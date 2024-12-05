c68c4668a4f93dc4e70428a734eedd1f884d8a0d
SFerFer1 <10048@holbertonstudents.com>
2024-12-05 14:12:47 +0000

    Hot fix

---
+++ file_io/1-create_file.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_file - check the code.
+ *
+ *@filename: Tamao del trianguo.
+ *@text_content: Tamao del trianguo.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int create_file(const char *filename, char *text_content)
+{
+
+
+
+
+
+}
