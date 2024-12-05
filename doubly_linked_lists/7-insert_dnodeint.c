a591e61bcb00ed665492940e6ebb7cfad409e204
SFerFer1 <10048@holbertonstudents.com>
2024-12-05 14:42:33 +0000

    Hot fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -26,6 +26,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	{
 		if (current->next == NULL)
 		{
+			if (cont == idx)
 			new = add_dnodeint_end(h, n);
 			return (new);
 		}
