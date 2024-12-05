bf7a7c8190f0fdcceddac5242fcc80fb694f965d
SFerFer1 <10048@holbertonstudents.com>
2024-12-05 14:39:16 +0000

    Hot fix

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,39 +13,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-unsigned int cont = 0;
-dlistint_t *current = *h;
-dlistint_t *new = NULL;
+	unsigned int cont = 0;
+	dlistint_t *current = *h;
+	dlistint_t *new = NULL;
 
-if (idx == 0)
-{
-new = add_dnodeint(h, n);
-return (new);
-}
-while (current != NULL)
-{
-if (current->next == NULL && cont == idx)
-{
-new = add_dnodeint_end(h, n);
-return (new);
-}
-else if (cont == idx)
-{
-new = malloc(sizeof(dlistint_t));
-if (new == NULL )
-return (NULL);
-new->n = n;
-new->next = current;
-new->prev = current->prev;
-if (current->prev != NULL)
-current->prev->next = new;
-current->prev = new;
-if (current == *h)
-*h = new;
-return (new);
-}
-current = current->next;
-cont++;
-}
-return (NULL);
+	if (idx == 0)
+	{
+		new = add_dnodeint(h, n);
+		return (new);
+	}
+	while (current != NULL)
+	{
+		if (current->next == NULL)
+		{
+			new = add_dnodeint_end(h, n);
+			return (new);
+		}
+		else if (cont == idx)
+		{
+			new = malloc(sizeof(dlistint_t));
+			if (new == NULL )
+				return (NULL);
+			new->n = n;
+			new->next = current;
+			new->prev = current->prev;
+			if (current->prev != NULL)
+				current->prev->next = new;
+			current->prev = new;
+			if (current == *h)
+				*h = new;
+			return (new);
+		}
+		current = current->next;
+		cont++;
+	}
+	return (NULL);
 }
